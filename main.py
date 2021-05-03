803ac2abcddd80a818aaf8fce228e39723023b88
finking <finking@list.ru>
2021-05-03 14:00:31 +0300

    Added schedule, two futures.

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from binance.client import Client
 from datetime import datetime
 import csv
 from Gsheets import Gsheet
+import schedule
+import time
 
 api_key = API_KEY
 api_secret = API_SECRET
 pathCSV = 'data/'
-maturity = '210625'
+maturities = ['210625', '210924']
 day_in_year = 365
 min_lot = 1
 price_range = 3  # количество знаков после запятой в цене.
@@ -18,42 +20,74 @@ client = Client(api_key, api_secret)
 
 def main():
     for spot_symbol in spot_symbols:
-        coin_futures_symbol = f'{spot_symbol}_{maturity}'
+        coin_futures_symbol_near = f'{spot_symbol}_{maturities[0]}'  # ближний фьючерс
+        coin_futures_symbol_further = f'{spot_symbol}_{maturities[1]}'  # дальний фьючерс
         # Книга заявок для спотового рынка
-        spot_order_book = client.get_order_book(symbol=spot_symbol+'T')
-        spot_best_price = get_best_price(spot_order_book)
-        print(f'Лучшая цена спота на покупку: {spot_best_price["bids"]}')
-        print(f'Лучшая цена спота на продажу: {spot_best_price["asks"]}')
+        print(f"Загрузка котировок для {spot_symbol}")
+        try:
+            spot_order_book = client.get_order_book(symbol=spot_symbol+'T')
+            spot_best_price = get_best_price(spot_order_book)
+            print(f'Лучшая цена спота на покупку: {spot_best_price["bids"]}')
+            print(f'Лучшая цена спота на продажу: {spot_best_price["asks"]}')
+        except Exception as e:
+            print(e)
 
         # Книга заявок для фьючерсов с базой в coin
-        coin_order_book = client.futures_coin_order_book(symbol=coin_futures_symbol)
-        # TODO разобраться как поменять cont (1 = 10USD) на coin (например на ETH)
-        coin_best_price = get_best_price(coin_order_book)
-        print(f'Лучшая цена фьючерса Coin на покупку: {coin_best_price["bids"]}')
-        print(f'Лучшая цена фьючерса Coin на продажу: {coin_best_price["asks"]}')
+        try:
+            coin_best_price_near = get_coin_futures_price(coin_futures_symbol_near, 'ближнего')
+            coin_best_price_further = get_coin_futures_price(coin_futures_symbol_further, 'дальнего')
+        except Exception as e:
+            print(e)
 
         # Вычисление количество дней до экспирации
-        maturity_date = datetime.strptime(maturity, "%y%m%d")
-        delta = maturity_date - datetime.today()
-        period = delta.days + 1
+        period_near = get_period(maturities[0])
+        period_further = get_period(maturities[1])
 
         # Вычисление доходности за период
-        yield_period = round(((coin_best_price["bids"]/spot_best_price["asks"])-1)*100, 2)
-        yield_year = round(yield_period/period*day_in_year,2)
+        try:
+            yield_period_near = round(((coin_best_price_near["bids"] / spot_best_price["asks"]) - 1) * 100, 2)
+            yield_year_near = round(yield_period_near / period_near * day_in_year, 2)
 
-        data = {'time': datetime.now().strftime("%d.%m.%Y %H:%M:%S"),
-                'spot_bids': spot_best_price["bids"],
-                'spot_ask': spot_best_price["asks"],
-                'coin_bids': coin_best_price["bids"],
-                'coin_asks': coin_best_price["asks"],
-                'period': period,
-                'yield_period (%)': yield_period,
-                'yield_year (%)': yield_year
-                }
+            yield_period_further = round(((coin_best_price_further["bids"] / spot_best_price["asks"]) - 1) * 100, 2)
+            yield_year_further = round(yield_period_further / period_further * day_in_year, 2)
 
+            data = {'time': datetime.now().strftime("%d.%m.%Y %H:%M:%S"),
+                    'spot_bids': spot_best_price["bids"],
+                    'spot_ask': spot_best_price["asks"],
+                    'coin_bids_near': coin_best_price_near["bids"],
+                    'coin_asks_near': coin_best_price_near["asks"],
+                    'period_near': period_near,
+                    'yield_period_near': yield_period_near,
+                    'yield_year_near': yield_year_near,
+                    'space': '',
+                    'coin_bids_further': coin_best_price_further["bids"],
+                    'coin_asks_further': coin_best_price_further["asks"],
+                    'period_further': period_further,
+                    'yield_period_further': yield_period_further,
+                    'yield_year_further': yield_year_further,
+                    }
+            write_google_sheets(spot_symbol, data)
+        except Exception as e:
+            print(f'Рачет доходности не удался! ERROR: {e}')
+    print('Ожидание времени запуска скрипта')
         # print(f"Запись в файл: {pathCSV}binance.csv")
         # write_csv(data)
-        writeGoogleSheets(spot_symbol, data)
+
+
+def get_period(maturity):
+    maturity_date = datetime.strptime(maturity, "%y%m%d")
+    delta = maturity_date - datetime.today()
+    period = delta.days + 1
+    return period
+
+
+def get_coin_futures_price(coin_futures_symbol, name):
+    coin_order_book = client.futures_coin_order_book(symbol=coin_futures_symbol)
+    # TODO разобраться как поменять cont (1 = 10USD) на coin (например на ETH)
+    coin_best_price = get_best_price(coin_order_book)
+    print(f'Лучшая цена {name} фьючерса Coin на покупку: {coin_best_price["bids"]}')
+    print(f'Лучшая цена {name} фьючерса Coin на продажу: {coin_best_price["asks"]}')
+    return coin_best_price
 
 
 def get_best_price(order_book):
@@ -77,18 +111,25 @@ def get_best_price(order_book):
 
 
 # Запись в Гугл.Таблицы
-def writeGoogleSheets(pair, data):
+def write_google_sheets(pair, data):
     sheet = Gsheet(pair)
     ws = sheet.worksheet
     try:
         ws.append_row([data['time'],
-                         data['spot_bids'],
-                         data['spot_ask'],
-                         data['coin_bids'],
-                         data['coin_asks'],
-                         data['period'],
-                         data['yield_period (%)'],
-                         data['yield_year (%)']])
+                        data['spot_bids'],
+                        data['spot_ask'],
+                        data['coin_bids_near'],
+                        data['coin_asks_near'],
+                        data['period_near'],
+                        data['yield_period_near'],
+                        data['yield_year_near'],
+                        data['space'],
+                        data['coin_bids_further'],
+                        data['coin_asks_further'],
+                        data['period_further'],
+                        data['yield_period_further'],
+                        data['yield_year_further'],
+                       ])
         print(f'Запись в Гугл.Таблицу по паре {pair} завершена успешна')
     except Exception as e:
         print(f'Запись в Гугл.Таблицу по {pair} не удалась')
@@ -96,4 +137,9 @@ def writeGoogleSheets(pair, data):
 
 
 if __name__ == '__main__':
-    main()
+    schedule.every().hour.at(":00").do(main)
+    print('Ожидание времени запуска скрипта')
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
